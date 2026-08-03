c5644ce508043312a8cda4017166bb1747b19da4
Ramnavathar <rameshwarnavathar16@gmail.com>
2026-08-03 10:06:23 +0530

    add paragraph

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
+const searchBtn = document.getElementById("searchBtn");
 
+searchBtn.addEventListener("click", () => {
+
+    const searchInput = document.getElementById("searchInput").value;
+
+    if (searchInput === "") {
+
+        alert("Please enter a recipe name!");
+
+    } else {
+
+        alert(`Searching for: ${searchInput}`);
+
+    }
+
+});
